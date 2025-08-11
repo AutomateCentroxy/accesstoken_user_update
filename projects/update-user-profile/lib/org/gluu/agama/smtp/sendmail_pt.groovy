f300a11da199d6e73a827a9d45036cb542c9913f
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-11 18:23:43 +0530

    feat: add sendmail_pt.groovy

---
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/sendmail_pt.groovy
@@ -0,0 +1,15 @@
+return [
+    subject: "O teu nome de utilizador foi criado com sucesso",
+    body: """
+Olá ${givenName ?: "utilizador"},
+Parabéns! O teu nome de utilizador foi criado com sucesso. 🔒
+Nome de utilizador: ${username}
+Agora já podes usar o teu nome de utilizador em vez do e-mail para iniciares sessão, tornando a tua experiência mais simples e segura.
+Mas isso não é tudo!
+Não estamos apenas a melhorar a forma como inicias sessão, estamos a preparar o terreno para algo entusiasmante. Novas funcionalidades estão a caminho, desenhadas para impulsionar o teu percurso rumo a um futuro financeiro mais próspero.
+O melhor ainda está para vir!
+Entretanto, se tiveres alguma dúvida ou precisares de ajuda, estamos apenas a um clique de distância.
+Com os melhores cumprimentos,
+Equipa Phi Wallet
+"""
+]
