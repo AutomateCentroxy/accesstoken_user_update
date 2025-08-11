803911ac73e775d9cf987e449b2bea02fbda14dc
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-11 19:16:18 +0530

    refactor: update SendEmailTemplate.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplate.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplate.groovy
@@ -7,17 +7,17 @@ import org.gluu.agama.smtp.jans.model.ContextData
 class SendEmailTemplate {
 
     private static String getTemplateContent(String templateName, String lang) {
-    String fileLang = lang?.toLowerCase() ?: "en"
-    String fileName = "${templateName}_${fileLang}.groovy"
-    InputStream is = SendEmailTemplate.class.getResourceAsStream("/org/gluu/agama/smtp/" + fileName)
-    if (is == null) {
-        is = SendEmailTemplate.class.getResourceAsStream("/org/gluu/agama/smtp/${templateName}_en.groovy")
+        String fileLang = lang?.toLowerCase() ?: "en"
+        String fileName = "${templateName}_${fileLang}.groovy"
+        InputStream is = SendEmailTemplate.class.getResourceAsStream("/org/gluu/agama/smtp/" + fileName)
+        if (is == null) {
+            is = SendEmailTemplate.class.getResourceAsStream("/org/gluu/agama/smtp/${templateName}_en.groovy")
+        }
+        if (is == null) {
+            throw new FileNotFoundException("Template file not found in classpath")
+        }
+        return is.text
     }
-    if (is == null) {
-        throw new FileNotFoundException("Template file not found in classpath")
-    }
-    return is.text
-}}
 
     static String get(String templateName, String username, String givenName, String lang, ContextData context) {
         File templateFile = getTemplateFile(templateName, lang)
