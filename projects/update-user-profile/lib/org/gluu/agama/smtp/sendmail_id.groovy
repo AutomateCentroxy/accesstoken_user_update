08c5c86632e3f1570ba6689816dc22734469063a
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-11 18:23:05 +0530

    feat: add sendmail_id.groovy

---
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/sendmail_id.groovy
@@ -0,0 +1,14 @@
+return [
+    subject: "Fitur baru! Klaim nama pengguna Anda hari ini",
+    body: """
+Halo ${givenName ?: "pengguna"},
+Kami sedang meningkatkan pengalaman login Anda. Semuanya dimulai dengan membuat nama pengguna Anda.
+Dengan mengatur nama pengguna, Anda akan mendapatkan akses masuk yang lebih cepat dan lebih aman. Ini juga akan mempersiapkan akun Anda untuk fitur-fitur baru yang akan segera hadir.
+Anda hanya perlu melakukannya satu kali, dan prosesnya sangat cepat.
+Tips: Lakukan sekarang untuk mengamankan nama pengguna favorit Anda sebelum digunakan oleh orang lain.
+[CTA: Buka Aplikasi]
+Butuh bantuan? Tim dukungan kami siap membantu Anda.
+Salam hangat,
+Tim Phi Wallet
+"""
+]
