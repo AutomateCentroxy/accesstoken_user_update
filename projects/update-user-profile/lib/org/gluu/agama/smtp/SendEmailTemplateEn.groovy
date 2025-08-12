0f1d43eebb58dcc445923e675b7b9d181ba209f1
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-12 18:00:52 +0530

    refactor: update SendEmailTemplateEn.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateEn.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateEn.groovy
@@ -12,12 +12,23 @@ class SendEmailTemplateEn {
 
         String html = """
 <div style="width: 640px; font-size: 18px; font-family: Arial, 'Segoe UI', Tahoma, sans-serif; font-weight: 300; color: #333;">
+
+    <!-- Logo at top -->
+    <div style="text-align: center; padding: 20px 0;">
+        <img src="https://phiwallet.com/components/images/logo.png" alt="Phi Logo" style="height: 40px;">
+    </div>
+    <hr style="border: none; border-top: 1px solid #ccc; margin: 0 0 20px 0;">
+
     <div style="padding: 12px; border-bottom: 1px solid #ccc;">
         <p>Dear User,</p>
         <p>Congratulations! Your username has been successfully created.</p>
-        <p>Username: <b>""" + username + """</b></p>
+
+        <p>Username: <span style="font-weight: bold;">""" + username + """</span></p>
+
         <p>You can now use your username instead of your email address to sign in, making your login experience smoother and more secure.</p>
-        <p><b>But that's not all!</b></p>
+
+        <p><span style="font-weight: bold;">But that's not all!</span></p>
+
         <p>We're not just upgrading how you log in, we're setting the stage for something exciting.
            Powerful new features are on the way, designed to help boost your journey toward a more prosperous financial future.</p>
         <p>Stay tuned, the best is yet to come!</p>
@@ -25,11 +36,7 @@ class SendEmailTemplateEn {
         <p>Kind regards,<br>Phi Wallet Team</p>
     </div>
 
-    <div style="background-color: #f9f9f9; padding: 20px; font-size: 14px; display: flex; justify-content: flex-start;">
-        <div>
-            <img src="https://phiwallet.com/components/images/logo.png" alt="Phi Logo" style="height: 40px;">
-        </div>
-    </div>
+    
 </div>
         """;
 
