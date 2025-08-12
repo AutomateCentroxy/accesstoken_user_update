ff06f5947de76e57b598ee51e541c29b16422931
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-12 18:20:50 +0530

    refactor: update SendEmailTemplateFr.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateFr.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateFr.groovy
@@ -12,10 +12,18 @@ class SendEmailTemplateFr {
 
         String html = """
 <div dir="ltr" lang="fr" style="width: 640px; font-size: 18px; font-family: Arial, 'Segoe UI', Tahoma, sans-serif; font-weight: 300; color: #333; text-align: left;">
+
+    <!-- Logo at top -->
+    <div style="text-align: center; padding: 20px 0;">
+        <img src="https://phiwallet.com/components/images/logo.png" alt="Phi Logo" style="height: 40px;">
+    </div>
+    <hr style="border: none; border-top: 1px solid #ccc; margin: 0 0 20px 0;">
     <div style="padding: 20px; border-bottom: 1px solid #ccc;">
         <p>Bonjour,</p>
         <p>Félicitations ! Votre nom d'utilisateur a été créé avec succès </p>
-        <p>Nom d'utilisateur : <b>""" + username + """</b></p>
+
+        <p>Nom d'utilisateur : <span style="font-weight: bold;">""" + username + """</span></p>
+        
         <p>Vous pouvez désormais utiliser votre nom d'utilisateur au lieu de votre adresse e-mail pour vous connecter, offrant ainsi une expérience plus fluide et sécurisée.</p>
         <p><b>Mais ce n'est pas tout !</b></p>
         <p>Nous ne faisons pas que simplifier votre connexion ; nous préparons aussi quelque chose d'excitant. De nouvelles fonctionnalités arriveront bientôt, conçues pour booster votre parcours vers un avenir financier plus prospère.</p>
@@ -26,11 +34,6 @@ class SendEmailTemplateFr {
 
 
 
-    <div style="background-color: #f9f9f9; padding: 20px; font-size: 14px; display: flex; justify-content: flex-start;">
-        <div>
-            <img src="https://phiwallet.com/components/images/logo.png" alt="Logo Phi" style="height: 40px;">
-        </div>
-    </div>
 </div>
         """;
 
