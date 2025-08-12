4610a8cc586ea706b7918aebe45b8b68e5f8a8fc
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-12 16:32:09 +0530

    refactor: update SendEmailTemplateEs.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateEs.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateEs.groovy
@@ -5,42 +5,42 @@ import java.time.format.DateTimeFormatter;
 import org.gluu.agama.smtp.jans.model.ContextData;
 import java.util.Map;
 
-class SendEmailTemplateEs {
+class SendEmailTemplateFr {
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy, hh:mma (O)");
 
     static Map<String, String> get(String username, ContextData context) {
 
         String html = """
-<div dir="ltr" lang="es" style="width: 640px; font-size: 18px; font-family: Arial, 'Segoe UI', Tahoma, sans-serif; font-weight: 300; color: #333; text-align: left;">
-    <div style="padding: 12px; border-bottom: 1px solid #ccc;">
-        <p>Hola,</p>
-        <p>¡Felicidades! Tu nombre de usuario ha sido creado con éxito.</p>
-        <p>Nombre de usuario: <b>""" + username + """</b></p>
-        <p>Ahora puedes usar tu nombre de usuario en lugar de tu correo electrónico para iniciar sesión, haciendo que tu experiencia sea más fluida y segura.</p>
-        <p><b>¡Pero eso no es todo!</b></p>
-        <p>No solo estamos mejorando tu forma de iniciar sesión, sino que estamos preparando tu cuenta para nuevas funciones diseñadas para impulsar tu camino hacia un futuro financiero más próspero.</p>
-        <p>¡Lo mejor está por venir!</p>
-        <p>Mientras tanto, si tienes alguna pregunta o necesitas asistencia, estamos a un clic de distancia.</p>
-        <p>Un saludo cordial,<br>Equipo de Phi Wallet</p>
+<div dir="ltr" lang="fr" style="width: 640px; font-size: 18px; font-family: Arial, 'Segoe UI', Tahoma, sans-serif; font-weight: 300; color: #333; text-align: left;">
+    <div style="padding: 20px; border-bottom: 1px solid #ccc;">
+        <p>Bonjour,</p>
+        <p>Félicitations ! Votre nom d'utilisateur a été créé avec succès </p>
+        <p>Nom d'utilisateur : <b>""" + username + """</b></p>
+        <p>Vous pouvez désormais utiliser votre nom d'utilisateur au lieu de votre adresse e-mail pour vous connecter, offrant ainsi une expérience plus fluide et sécurisée.</p>
+        <p><b>Mais ce n'est pas tout !</b></p>
+        <p>Nous ne faisons pas que simplifier votre connexion ; nous préparons aussi quelque chose d'excitant. De nouvelles fonctionnalités arriveront bientôt, conçues pour booster votre parcours vers un avenir financier plus prospère.</p>
+        <p>Restez à l'écoute, le meilleur est à venir !</p>
+        <p>Entre-temps, si vous avez des questions ou besoin d'assistance, nous sommes à un clic de distance.</p>
+        <p>Cordialement,<br>L'équipe Phi Wallet</p>
     </div>
 
     <div style="padding: 12px; background-color: #ecf0f5; font-size: 16px;">
-        <p style="color: #48596b; font-weight: 500;">Fecha del evento:</p>
-        <p><span style="color: #48596b; font-weight: 500;">Fecha:</span><br>""" + computeDateTime(context.getTimeZone()) + """</p>
+        <p style="color: #48596b; font-weight: 500;">Date de l'événement :</p>
+        <p><span style="color: #48596b; font-weight: 500;">Date :</span><br>""" + computeDateTime(context.getTimeZone()) + """</p>
         
     </div>
 
     <div style="background-color: #f9f9f9; padding: 20px; font-size: 14px; display: flex; justify-content: flex-start;">
         <div>
-            <img src="https://phiwallet.com/components/images/logo.png" alt="Phi Logo" style="height: 40px;">
+            <img src="https://phiwallet.com/components/images/logo.png" alt="Logo Phi" style="height: 40px;">
         </div>
     </div>
 </div>
         """;
 
         return Map.of(
-            "subject", "Tu nombre de usuario ha sido creado correctamente",
+            "subject", "Votre nom d'utilisateur a été créé avec succès",
             "body", html
         );
     }
