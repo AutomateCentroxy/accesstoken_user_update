4c3bccd11088de5da1d18bec2ce51384abe98988
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-13 15:42:13 +0530

    refactor: update SendEmailTemplateAr.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateAr.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/SendEmailTemplateAr.groovy
@@ -58,7 +58,7 @@ class SendEmailTemplateAr {
                     <tr>
                       <td style="padding:20px;font-size:12px;color:#666;">
                         <div style="direction:ltr; display:inline-block; text-align:left;">
-                          <p style="margin:0 0 10px 0;font-size:14px;font-weight:bold;color:#565555; direction:rtl; text-align:right;">تابعنا على:</p>
+                          <p style="margin:0 0 10px 0;font-size:14px;font-weight:bold;color:#565555;">تابعنا على:</p>
                           <p style="margin:0;">
                             <a href="https://www.facebook.com/PhiWallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/facebook.png" style="height:20px;"></a>
                             <a href="https://x.com/PhiWallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/twitter.png" style="height:20px;"></a>
